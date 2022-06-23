70b40d2f7aff30813b12e491c1748dadf7aa3e34
shruthi093 <jayasruthi.l@gmail.com>
2022-06-23 11:02:04 +0530

    js basics

---
+++ scripts/jsbasics.js
@@ -0,0 +1,63 @@
+console.log("JS session 1")
+//comment
+/**
+ * some comments
+ * 
+ * s
+ * dsd
+ * s
+ * ds
+ * 
+ */
+/*let fname="john"
+var age=20.00007
+
+console.log(typeof(fname))
+console.log(typeof(age))
+console.log("before",fname)
+console.log("before",age)
+
+fname="johan"
+age=21
+
+console.log("after",fname)
+console.log("after",age)
+
+const salary=40000
+console.log("before",salary)
+
+let backlogs=0
+console.log(typeof(backlogs))
+
+//arrays
+
+let studentDetails=["john",20,9.0,false]
+console.log(`the student name is ${studentDetails[0]} and the age is ${studentDetails[1]}`)
+
+//objects
+
+let studentDetails={
+    fname:"john",
+    age:20,
+    cgpa:9.0,
+    backlogs:false
+}
+
+console.log(` the student name is${studentDetails.fname} `)*/
+
+let a=10
+let b=10
+
+console.log(a===b)
+
+let c=20
+let d="20"
+console.log(c!==d)
+
+
+
+
+
+
+
+
