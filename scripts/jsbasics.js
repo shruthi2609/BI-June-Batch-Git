4757af7a756eed8b40485a89ba7a3f19c8d5c099
shruthi093 <jayasruthi.l@gmail.com>
2022-06-24 11:08:08 +0530

    ops and loops , conditionals

--- scripts/jsbasics.js
+++ scripts/jsbasics.js
@@ -43,7 +43,7 @@ let studentDetails={
     backlogs:false
 }
 
-console.log(` the student name is${studentDetails.fname} `)*/
+console.log(` the student name is${studentDetails.fname} `)
 
 let a=10
 let b=10
@@ -53,6 +53,82 @@ console.log(a===b)
 let c=20
 let d="20"
 console.log(c!==d)
+let a=10
+console.log(a--)
+console.log(a)
+let a=10
+let b=5
+a&&=b
+console.log(a)
+
+let fname;
+let lname;
+
+console.log(fname??lname??"user")
+let age=25
+let surgeries=false
+let covid=true
+
+
+if((age>=18&&age<=60)&&!surgeries&&!covid){
+    console.log("if statement")
+}
+else{
+    console.log("else")
+}
+
+if(cond){
+
+}
+else if(cond){
+
+}
+else{
+
+}
+
+let day=90
+switch(day){
+    case 0:
+        console.log("sunday")
+        break
+    case 1:
+        console.log("monday")
+        break
+    case 2:
+        console.log("tuesday")
+        break
+    default:
+        console.log("no match")
+
+}
+
+for(var i=0;i<=10;i++){
+    if(i%2==0){
+        console.log(i)
+    }
+}
+console.log("outside",i)//not allowed use let to define i
+
+let j=10
+while(j>=100){
+    console.log("while",j)
+    j++
+}
+
+do{
+    console.log("do while",j)
+    j++
+}while(j>=100)*/
+
+let studentReg=[1001,1002,1003,1004]
+for(let i=0;i<=3;i++){
+    console.log(studentReg[i])
+}
+
+
+
+
 
 
 
