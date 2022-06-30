179b77c63685a5aa76b7d6eff72787cee79175d8
shruthi093 <jayasruthi.l@gmail.com>
2022-06-30 11:00:24 +0530

    strings and array methods

---
+++ scripts/arraymethods.js
@@ -0,0 +1,23 @@
+const arr1=["john","henry","peter"]
+/*const arr2=["helen","mary"]
+const arr3=["texas","austin"]
+const arr4=arr1.concat(arr2,arr3)
+console.log(arr4)
+console.log(arr1.slice(-3,-1))
+console.log(arr1.includes("mary"))*/
+
+/*console.log(arr.toString().replaceAll(","," "))
+const str=arr.toString()
+console.log(str.replaceAll(","," "))
+console.log(arr.join(" "))*/
+/*arr.push("mary")
+arr.push("johan")
+arr.pop()
+arr.pop()
+arr.pop()
+arr.unshift("mary")
+arr.shift()
+console.log(arr)*/
+
+
+
--- scripts/strings.js
+++ scripts/strings.js
@@ -1,4 +1,4 @@
-let username='john henry'
+/*let username='john henry'
 console.log(username[0])
 
 let str = "Please locate where 'locate' occurs!";
@@ -6,4 +6,19 @@ console.log(str.indexOf("js",8))
 
 let text = "Please visit Microsoft and Microsoft!";
 let newText = text.replace("Microsoft", "W3Schools");  // Returns Please visit W3Schools and W3Schools!
-console.log(newText)
+console.log(newText)*/
+
+/*let inputstr="Please visit"
+//console.log(inputstr.slice(7))
+console.log(inputstr.slice(-12,-6))
+console.log(inputstr.substr(0,6))
+let a="Hello"
+let b="0"
+console.log(a+","+b)
+console.log(a.concat(",",b))
+let str="Hello Javascript"
+console.log(str.split(" "))*/
+
+
+
+
