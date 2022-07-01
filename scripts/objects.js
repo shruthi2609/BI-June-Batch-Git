89ed27c696d07610c0c762c5ecf662fd258c9fae
shruthi093 <jayasruthi.l@gmail.com>
2022-07-01 11:02:51 +0530

    arrays and objects

---
+++ scripts/objects.js
@@ -0,0 +1,20 @@
+let obj={
+    fname:"henry",
+    age:20,
+    cgpa:9.0,
+    getDetails:function(){
+        console.log(this.fname,this.cgpa)
+    }
+}
+obj.address="IND"
+
+
+/*console.log(obj)
+obj.getDetails()
+for(let property in obj){
+    console.log(property,obj[property])
+}
+console.log(Object.keys(obj))
+console.log(Object.values(obj))
+console.log(Object.entries(obj))*/
+
