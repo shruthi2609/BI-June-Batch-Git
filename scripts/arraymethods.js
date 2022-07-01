1fa6496fefcb1e50620449f99a18b45cde2a69d6
shruthi093 <jayasruthi.l@gmail.com>
2022-07-01 11:02:38 +0530

    arrays and objects

--- scripts/arraymethods.js
+++ scripts/arraymethods.js
@@ -18,6 +18,108 @@ arr.pop()
 arr.unshift("mary")
 arr.shift()
 console.log(arr)*/
+/*const arr2=["john","henry","peter","ann","ben"]
+console.log(arr2.sort())
+
+const arr3=[40, 100, 1, 5, 25, 10]
+console.log(arr3.sort(
+//(a,b)=>b-a ->descending
+//(a,b)=>a-b ->ascdending
+(a,b)=>{
+    if(a>b){
+        return 1
+        //return -1 // desceding
+    }
+    if(a<b){
+        return -1 //ascending
+    }
+    return 0
+}
+))*/
+const arr2=["john","henry","peter","ann","ben"]
+/*for(let i=0;i<arr2.length;i++){
+    console.log(arr2[i])
+}
+let result="";
+arr2.forEach(
+    (item)=>{
+        result=result+" "+item
+        
+    }
+)
+console.log(result)
+
+console.log("for loop")
+for(let i=0;i<arr2.length;i++){
+    if(arr2[i].length>0) 
+    {
+        console.log(arr2[i])
+    }
+    else{
+        console.log("empty item found")
+    }
+}
+
+console.log("foreach loop")
+arr2.forEach(
+    (item)=>{
+       if(item.length>0){
+        console.log(item)
+       }
+       else{
+        console.log("empty item found")
+       }
+    }
+)
+
+const result=arr2.map(
+    (item)=>item+" "+"S"
+    // henry S
+    //peter S
+)
+console.log(result)
+
+const result1=arr2.find(
+    (item)=>item.length===3
+)
+console.log(result1)
+
+const result2=arr2.filter(
+    (item)=>item.length===3
+)
+console.log(result2)
+
+const result=arr2.findIndex(
+    (item)=>item==="ann"
+)
+console.log(result)
+
+const evr=arr2.every((item)=>item.length>=3)
+const som=arr2.some((item)=>item.length>=3)
+
+console.log(evr)
+console.log(som)*/
+
+
+
+// [1,4,5,68,90,0]
+
+1. to check whether the array is having a negative No
+2.to print all the even nos in the give array 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
