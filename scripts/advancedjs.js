8a58236edfc3a56b9f66ee86d6eab8d001d7517d
shruthi093 <jayasruthi.l@gmail.com>
2022-07-19 12:02:29 +0530

    ajax

---
+++ scripts/advancedjs.js
@@ -0,0 +1,32 @@
+//es6 functions
+//function expression
+//arrow functions
+/*const greetUser=(username)=>{
+console.log(`welcome ${username}`)
+console.log("end")
+}
+greetUser("john")
+//callback
+function add(a,b){
+    return a+b
+}
+
+function sum(fun){
+console.log(fun(10,5))
+}
+
+sum(add)
+let obj={
+    fname:"john",
+    email:"john@gmail.com",
+    phone:902930222,
+    getUsers:function(){
+       console.log(this.fname,this.email,this.phone)
+    }
+}
+obj.getUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users').then((res)=>{return res.json()}).then((res)=>console.log(res))*/
+
+
+
